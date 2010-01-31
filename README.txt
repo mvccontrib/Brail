372d15d3da9dd64bb9c1fe758de3231c2df009e9
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2010-01-31 17:21:18 +0000

    Importing brail source from main mvccontrib repo.

---
+++ README.txt
@@ -0,0 +1,4 @@
+This is a port of the Castle Project's Brail view engine for use with ASP.NET MVC.
+http://castleproject.org
+
+This port is no longer being actively maintained.
---
+++ src/MvcContrib.BrailViewEngine/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: ComVisibleAttribute(false)]
+[assembly: AssemblyVersionAttribute("2.0.0.0")]
+[assembly: AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: AssemblyCopyrightAttribute("Copyright © MVC Contrib 2007-2010")]
+[assembly: AssemblyProductAttribute("MVCContrib 2.0")]
+[assembly: AssemblyCompanyAttribute("MVC Contrib")]
+[assembly: AssemblyConfigurationAttribute("release")]
+[assembly: AssemblyInformationalVersionAttribute("2.0.0.0")]
